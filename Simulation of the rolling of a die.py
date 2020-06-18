29bfd26c8b083f53c7a25dc1f599b33a3566e6b2
103038923 <103038923@student.swin.edu.au>
2020-06-18 19:12:17 +1000

    Create Simulation of the rolling of a die.py

---
+++ Simulation of the rolling of a die.py
@@ -0,0 +1,21 @@
+
+sum = []
+i = 1
+min_number = 1
+max_number = 6
+ht = int(input("How many times do you want to roll the dice :"))
+while i <= ht :
+    import random
+    num = int(random.randint(min_number,max_number))
+    print("Rolling the dice")
+    print("The number randomly generated is :",num)
+    sel = str(input("Indicate whether you want this to be counted (y or n) "))
+    if sel == 'y' :
+        sum.append(num)
+        print("the number",num,"is selected")
+    i += 1
+print("The sum of all number is",ht)
+if sum == [] :
+    print("None of the numbers were selected.")
+else:
+    print("The numbers selected are",sum)
